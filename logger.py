eabab97d9a58365c5ec036a87b08dcc91c093713
Chr1s2564 <chris.dli@proton.me>
2026-01-31 18:00:14 +0100

    Added: Screen dimensions in constants.py, logger_setup in logger.py

---
+++ logger.py
@@ -0,0 +1,26 @@
+import logging
+import sys
+
+def logger_setup():
+    #logger Config
+    logger = logging.getLogger("Game")
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+    #logger destination
+    console_out = logging.StreamHandler(sys.stdout)
+    console_out.setLevel(logging.DEBUG) # maybe modify to .INFO
+    console_out.setFormatter(formatter)
+
+    #log file writing
+    file_out = logging.FileHandler("logs.txt")
+    file_out.setLevel(logging.DEBUG)
+    file_out.setFormatter(formatter)
+
+    logger.addHandler(console_out)
+    logger.addHandler(file_out)
+
+    return logger
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pygame
+from sys import argv
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import logger_setup
+
 
 pygame.init()
+
+def main():
+    if argv[1] == "debug":
+        logger_setup()
+    if argv[1] != "debug":
+        pass
+    print(f"Runing pygame version: {pygame.version}")
+
+
+
+if __name__ == "__main__":
+    main()
