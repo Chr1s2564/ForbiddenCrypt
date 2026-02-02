f5998aa525ab51f5743a42ad3fea50a22b3d4dd3
Chr1s2564 <chris.dli@proton.me>
2026-02-02 11:43:10 +0100

    Added: player sprite draw to center of the room

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from sys import argv
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import logger_setup
 from colors import *
+from player import Player
 
 def main():
     if argv[1] == "debug":
@@ -13,17 +14,17 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #rectangle = pygame.Rect(13, 7, SCREEN_WIDTH, SCREEN_HEIGHT)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BLACK)
-        #pygame.draw.rect(screen, GREY, rectangle)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
+gi
 
 
 
--- src/player.py
+++ src/player.py
@@ -1,8 +1,10 @@
 import pygame
+import os
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
-import os
 
+
+#path to sprites // currently a test sprite
 base_path = os.path.dirname(__file__)
 image_path = os.path.join(base_path, "..", "sprites", "test_sprite.png")
 
@@ -11,6 +13,10 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (96, 96))
+        self.original_image = self.image
 
-    def draw(self):
-        pass
+    def draw(self, screen):
+        rotated = pygame.transform.rotate(self.original_image, -self.rotation)
+        rect = rotated.get_rect(center=self.position)
+        screen.blit(rotated, rect)
